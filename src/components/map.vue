b3ae6f99785003d29394d0dc472182cd05e5ca69
Vincent <makokhavomondi@gmail.com>
2024-06-07 18:56:07 +0300

    Initial vite app

---
+++ src/components/map.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="map">
+    Amazing stuff!!
+  </div>
+</template>
+
+<script>
+export default {
+  name: map,
+}
+</script>
+
+<style>
+.map {
+  outline: solid 0.1rem red;
+}
+</style>
